fb49932359f17e35876bcd44967b24bd728efbaa
AsadurRubel <miarubel04@.com>
2024-03-28 23:44:53 +0600

    Finaly Complete the project

--- src/Components/PagesToRead/PagesToRead.jsx
+++ src/Components/PagesToRead/PagesToRead.jsx
@@ -1,8 +1,50 @@
-import PropTypes from 'prop-types';
+
 import { useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import { getStoredBookList } from "../LocalStorage/saveLocalStorage";
-import BarChart from "../BarChart/BarChart";
+import React from 'react';
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid } from 'recharts';
+
+const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red', 'pink'];
+
+
+const getPath = (x, y, width, height) => {
+  return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3}
+  ${x + width / 2}, ${y}
+  C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${x + width}, ${y + height}
+  Z`;
+};
+
+const TriangleBar = (props) => {
+  const { fill, x, y, width, height } = props;
+
+  return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 const PagesToRead = () => {
@@ -25,16 +67,28 @@ const PagesToRead = () => {
     }
 },[books]);
 
-    return (
-        <div className="flex">
-            {
-                chart.map((chart, inx) => <BarChart key={inx} chart={chart}> </BarChart> )
-            }
-        </div>
-    );
-};
-PagesToRead.propTypes = {
-    books: PropTypes.array.isRequired,
-    
-  };
+return (
+    <BarChart
+      width={500}
+      height={300}
+      data={chart}
+      margin={{
+        top: 20,
+        right: 30,
+        left: 20,
+        bottom: 5,
+      }}
+    >
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="bookName" />
+      <YAxis dataKey="totalPages" />
+      <Bar dataKey="totalPages" fill="#8884d8" shape={<TriangleBar />} label={{ position: 'top' }}>
+        {chart.map((entry, index) => (
+          <Cell key={`cell-${index}`} fill={colors[index % 20]} />
+        ))}
+      </Bar>
+    </BarChart>
+  );
+
+    }
 export default PagesToRead;
